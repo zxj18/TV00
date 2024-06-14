baf1711c19de01d1a9c945424f4b0787c531a443
okjack <okjackcaptain@gmail.com>
2024-06-14 11:27:33 +0800

    Update ConfigDao.java

--- app/src/main/java/com/fongmi/android/tv/db/dao/ConfigDao.java
+++ app/src/main/java/com/fongmi/android/tv/db/dao/ConfigDao.java
@@ -15,7 +15,7 @@ public abstract class ConfigDao extends BaseDao<Config> {
     public abstract List<Config> findByType(int type);
 
     @SuppressWarnings(RoomWarnings.CURSOR_MISMATCH)
-    @Query("SELECT id, url, type, time FROM Config WHERE type = :type ORDER BY time DESC")
+    @Query("SELECT id, name, url, type, time FROM Config WHERE type = :type ORDER BY time DESC")
     public abstract List<Config> findUrlByType(int type);
 
     @Query("SELECT * FROM Config WHERE id = :id")
